4e6b6acb67ff5a0f2f1d5fb64c9bf666fd4e20e9
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-08-24 11:43:31 -0400

    fix lintern errors

--- src/App.js
+++ src/App.js
@@ -2,19 +2,19 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-} from "react-router-dom";
-import BookList from "./components/BookList/BookList";
-import Categories from "./components/Categories/Categories";
-import Header from "./components/Header/Header";
+} from 'react-router-dom';
+import BookList from './components/BookList/BookList';
+import Categories from './components/Categories/Categories';
+import Header from './components/Header/Header';
 
 function App() {
   return (
     <Router>
-        <Header />
-        <Switch>
-          <Route path="/" exact component={BookList}/>
-          <Route path='/categories' component={Categories}/>
-        </Switch>
+      <Header />
+      <Switch>
+        <Route path="/" exact component={BookList} />
+        <Route path="/categories" component={Categories} />
+      </Switch>
     </Router>
   );
 }
