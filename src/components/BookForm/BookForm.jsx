c709a5d32f1bd427c5696c178a235dd02c8416f2
Rodrigo Ibaceta <rodrigo.ibaceta01@gmail.com>
2021-08-24 18:31:40 -0400

    Create books duck

--- src/components/BookForm/BookForm.jsx
+++ src/components/BookForm/BookForm.jsx
@@ -1,11 +1,32 @@
-const BookForm = () => (
-  <div className="book-form">
-    <input type="text" className="book-input-name" />
-    <select name="book-categories" className="book-categories">
-      <option value="thriller">Thriller</option>
-    </select>
-    <button type="button">Add Book</button>
-  </div>
-);
+// import useDispatch hook
+import { useDispatch } from 'react-redux';
+// import your Action Creators
+import { addBook } from '../../redux/books/books';
+
+const dispatch = useDispatch();
+
+const BookForm = () => {
+  const submitBookToStore = () => {
+    const newBook = {
+      id, // make sure it's unique
+      title,
+      author,
+    };
+
+    // dispatch an action and pass it the newBook object (your action's payload)
+    dispatch(addBook(newBook));
+  };
+
+
+  return (
+    <div className="book-form">
+      <input type="text" className="book-input-name" />
+      <select name="book-categories" className="book-categories">
+        <option value="thriller">Thriller</option>
+      </select>
+      <button type="button" onClick={submitBookToStore}>Add Book</button>
+    </div>
+  );
+};
 
 export default BookForm;
